8b0d7b39c72edce9b36730ca77642213c23a94e6
don.abrams <don.abrams@sympoz.com>
2015-09-23 11:46:35 -0600

    initial commit

---
+++ fixture.js
@@ -0,0 +1,5 @@
+import * as a from 'b';
+import c from 'd';
+import {e as f} from 'g';
+import recast from 'recast';
+import h from './i';
---
+++ rules/require-in-package.js
@@ -0,0 +1,36 @@
+var findup = require('findup-sync');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(context) {
+	return {
+		"ImportDeclaration": function(node) {
+			var dependencyName = node.source.value;
+			// Don't worry about relative paths
+			if (dependencyName.indexOf('.') == 0) {
+				return;
+			}
+			var basedir = path.dirname(context.getFilename());
+			var packagePath = findup('package.json', {cwd: basedir});
+			// Don't worry if there is no package.json
+			if (packagePath) {
+				var pkg = JSON.parse(fs.readFileSync(packagePath));
+				var dependencies = [];
+				if (pkg.dependencies) {
+					dependencies = dependencies.concat(Object.keys(pkg.dependencies));
+				}
+				if (pkg.peerDependencies) {
+					dependencies = dependencies.concat(Object.keys(pkg.peerDependencies));
+				}
+				if (pkg.devDependencies) {
+					dependencies = dependencies.concat(Object.keys(pkg.devDependencies));
+				}
+				if (dependencies.indexOf(dependencyName) === -1) {
+					context.report(node, '`{{dependency}}` : is not a dependency in the local package.json', {
+						dependency: node.source.value
+					});
+				}
+			}
+		}
+	};
+};
