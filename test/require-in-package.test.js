c2404e527b18212da66f7c48886d1af015236178
don.abrams <don.abrams@sympoz.com>
2015-09-23 13:24:05 -0600

    add tests

---
+++ test/require-in-package.test.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var rule = require("../lib/rules/require-in-package"),
+    RuleTester = require("eslint").RuleTester;
+
+var ruleTester = new RuleTester();
+
+
+var valid = [
+    "import eslint from 'eslint';",
+    "import h from './i';",
+].map(function(code) {
+    return {
+        code: code,
+        parser: "babel-eslint",
+        ecmaFeatures: { "modules": true },
+    };
+});
+
+var message = "dependency not in the local package.json";
+
+var invalid = [
+    "import * as a from 'b';",
+    "import c from 'd';",
+    "import {e as f} from 'g';",
+].map(function(code) {
+    return {
+        code: code,
+        parser: "babel-eslint",
+        ecmaFeatures: { "modules": true },
+        errors: [{ message: message }],
+    };
+});
+
+ruleTester.run("require-in-package", rule, {
+    valid: valid,
+    invalid: invalid
+});
--- tests/lib/rules/fixture.js
+++
@@ -1,5 +0,0 @@
-import * as a from 'b';
-import c from 'd';
-import {e as f} from 'g';
-import recast from 'recast';
-import h from './i';
